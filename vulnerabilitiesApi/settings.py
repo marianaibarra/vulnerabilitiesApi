3823df4653c5714a1ed45754b76204ba6d8ae929
Mariana Z. (MacBook Air M1) <marian4ibarr4@gmail.com>
2025-01-26 14:46:27 -0500

    Set DEBUG to False and allow all hosts in settings.py for production readiness

--- vulnerabilitiesApi/settings.py
+++ vulnerabilitiesApi/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=xts&fv&!z#vtt$vq_$droux43&m@59)i)cp3wy(u*x)@sp8z&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
