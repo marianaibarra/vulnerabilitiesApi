4a569429249b675f149f6fc101178fad8f38fb89
Mariana Z. (MacBook Air M1) <marian4ibarr4@gmail.com>
2025-01-25 22:20:56 -0500

    Increase maximum length of description field in Vulnerability model to 1000 characters

--- vulnerabilities/models.py
+++ vulnerabilities/models.py
@@ -25,7 +25,7 @@ class Vulnerability(models.Model):
     cveId = models.CharField(max_length=100, unique=True)
     published = models.DateTimeField()
     vulnStatus = models.CharField( max_length=50, choices=VULNERABILITY_STATUS, default='Received')
-    description = models.CharField(max_length=200, default='N/A')
+    description = models.CharField(max_length=1000, default='N/A')
     hasBeenFixed = models.BooleanField(default=False)
     baseSeverityMetric = models.CharField(max_length=100, default='N/A', choices=BASE_SEVERITY_METRIC)
     
